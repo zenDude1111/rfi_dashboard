a14e6bf8bfea41ce91869f18025c93e5278d11bf
zendude <ericluttrell08@gmail.com>
2024-06-04 14:50:53 -0600

    edit

--- app.py
+++ app.py
@@ -2,13 +2,12 @@ from datetime import date
 import os
 import pandas as pd
 import requests
-from io import StringIO
-from dash import Dash, dcc, html, Input, Output, dash_table
+from dash import Dash, dcc, html, Input, Output
 import dash_bootstrap_components as dbc
 
 # Modularized layout imports
 from pages import frequency_explorer
-#from pages import known_frequencies2
+# from pages import known_frequencies2
 
 # Create Dash application instance with external stylesheets for theming
 app = Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.DARKLY])
@@ -20,7 +19,7 @@ def create_navbar():
         children=[
             dbc.NavItem(dbc.NavLink("Main", href="/")),
             dbc.NavItem(dbc.NavLink("Frequency Explorer", href="/frequency_explorer")),
-            #dbc.NavItem(dbc.NavLink("Known Frequencies", href="/known_frequencies")),
+            # dbc.NavItem(dbc.NavLink("Known Frequencies", href="/known_frequencies")),
         ],
         brand="South Pole RF Environment Dashboard",
         brand_href="/",
@@ -28,58 +27,39 @@ def create_navbar():
         dark=True,
     )
 
-def create_data_table():
-    """Initializes and returns an empty DataTable component."""
-    initial_df = pd.DataFrame()
-    return dash_table.DataTable(
-        id='main-rfi-data-table',
-        columns=[{"name": i, "id": i} for i in initial_df.columns],
-        data=initial_df.to_dict('records'),
-        style_cell={'textAlign': 'left', 'padding': '5px'},
-        style_header={
-            'backgroundColor': 'rgb(30, 30, 30)',
-            'color': 'white',
-            'fontWeight': 'bold'
-        },
-        style_data={
-            'backgroundColor': 'rgb(50, 50, 50)',
-            'color': 'white'
-        },
-        style_table={'overflowX': 'auto'}
-    )
-
 def layout_main_page():
     """Defines the main page layout using Bootstrap utility classes."""
     return html.Div([
         dbc.Row([
             dbc.Col([
-                html.P('Select a date:', className='mb-2'),  # mb-2: margin-bottom 2
-                dcc.DatePickerSingle(
-                    id='main-rfi-date-picker', #namespace id
-                    min_date_allowed=date(1995, 8, 5),
-                    max_date_allowed=date.today(),
-                    initial_visible_month=date.today(),
-                    date=date.today(),
-                    className='w-100',  # w-100: width 100%
-                ),
-            ], width=12, lg=3),
-            dbc.Col([
-                dcc.Tabs(
-                    id="main-image-tabs", 
-                    value='tab-1', 
-                    children=[
-                        dcc.Tab(label='SH1-MAPO', value='tab-1', className='text-white bg-secondary fw-bold'),
-                        dcc.Tab(label='SH1-DSL', value='tab-2', className='text-white bg-secondary fw-bold'),
-                        dcc.Tab(label='Signal Hound 3', value='tab-3', className='text-white bg-secondary fw-bold'),
-                    ],
-                    className='w-100 fs-5',  # w-100: width 100%, fs-5: font-size 5
-                ),
-            ], width=12, lg=9),
-        ]),
-        dbc.Row(dbc.Col(html.Div(id='main-tabs-content'), width=12)),
-        dbc.Row(dbc.Col([html.H3('RF Environment Report', className='mt-3'), create_data_table()], width=12)),
-    ], className='mt-3')  # mt-3: margin-top 3
-
+                dbc.Card(
+                    dbc.CardBody([
+                        html.P('Select a date:', className='mb-2'),
+                        dcc.DatePickerSingle(
+                            id='main-rfi-date-picker',
+                            min_date_allowed=date(1995, 8, 5),
+                            max_date_allowed=date.today(),
+                            initial_visible_month=date.today(),
+                            date=date.today(),
+                            className='w-100'
+                        ),
+                        dcc.Tabs(
+                            id="main-image-tabs", 
+                            value='tab-1', 
+                            children=[
+                                dcc.Tab(label='SH1-MAPO', value='tab-1', className='text-white bg-secondary fw-bold'),
+                                dcc.Tab(label='SH2-DSL', value='tab-2', className='text-white bg-secondary fw-bold'),
+                                dcc.Tab(label='Anritsu-DSL', value='tab-3', className='text-white bg-secondary fw-bold'),
+                                dcc.Tab(label='Compare', value='tab-4', className='text-white bg-secondary fw-bold'),
+                            ],
+                            className='w-100 fs-5'
+                        ),
+                        html.Div(id='main-tabs-content', style={"minHeight": "400px", "display": "flex", "alignItems": "center", "justifyContent": "center"})
+                    ])
+                )
+            ], width=12)
+        ], className='mb-3')
+    ], className='mt-3')
 
 app.layout = html.Div([
     dcc.Location(id='main-url', refresh=False),
@@ -93,53 +73,79 @@ app.layout = html.Div([
 )
 def update_image_tab_content(tab, selected_date):
     if not selected_date:
-        return html.Div("Select a date.")
+        return html.Div("Select a date.", style={"textAlign": "center", "color": "red", "fontWeight": "bold"})
 
     selected_date_obj = pd.to_datetime(selected_date)
     formatted_date = selected_date_obj.strftime('%Y%m%d')  # Format date as YYYYMMDD
+
+    if tab == 'tab-4':  # Compare tab
+        return html.Div([
+            dcc.Tabs(
+                id="compare-tabs",
+                value='compare-1',
+                children=[
+                    dcc.Tab(label='SH1-MAPO', value='compare-1', className='text-white bg-secondary fw-bold'),
+                    dcc.Tab(label='SH2-DSL', value='compare-2', className='text-white bg-secondary fw-bold'),
+                    dcc.Tab(label='Anritsu-DSL', value='compare-3', className='text-white bg-secondary fw-bold'),
+                ],
+                className='w-100 fs-5'
+            ),
+            html.Div(id='compare-tabs-content', style={"minHeight": "400px", "display": "flex", "alignItems": "center", "justifyContent": "center"})
+        ], style={"width": "100%"})
     
-    # 'tab' is structured in a way that appending its value directly maps to the correct png file
-    # Dynamically update image source paths to point to your Flask application
-    img_src = f"http://universe.phys.unm.edu/data/png/sh{tab[-1]}_{formatted_date}.png"
-    
-    return html.Div(
-        html.Img(src=img_src, style={"width": "100%", "height": "auto"}),
-        style={"maxWidth": "1200px", "margin": "0 auto"}
-    )
-
-@app.callback(
-    [Output('main-rfi-data-table', 'data'), Output('main-rfi-data-table', 'columns')],
-    [Input('main-image-tabs', 'value'), Input('main-rfi-date-picker', 'date')]
-)
-def update_data_table(tab, selected_date):
-
-    #current date formatted as yyyymmdd
-    current_date = date.today().strftime('%Y%m%d')
-
-    if not selected_date or not tab:
-        return [1], [current_date]
-
-    selected_date_obj = pd.to_datetime(selected_date)
-    formatted_date = selected_date_obj.strftime('%Y%m%d')  # Format date as YYYYMMDD
-    
-    # 'tab' is structured in a way that appending its value directly maps to the correct CSV file
-    csv_url = f'http://universe.phys.unm.edu/data/rfi_reports/sh{tab[-1]}_{formatted_date}_rfi_report.csv'
+    #check what tab is selected and edit the image source accordingly
+    if tab == 'tab-1':
+        img_src = f"http://universe.phys.unm.edu/data/waterfall/sh1_{formatted_date}.png"
+    elif tab == 'tab-2':
+        img_src = f"http://universe.phys.unm.edu/data/waterfall/sh2_{formatted_date}.png"
+    elif tab == 'tab-3':
+        img_src = f"http://universe.phys.unm.edu/data/waterfall/anritsu_{formatted_date}.png"
 
     try:
-        response = requests.get(csv_url)
+        response = requests.get(img_src)
         if response.status_code == 200:
-            # If the request was successful, parse the CSV data
-            df = pd.read_csv(StringIO(response.text))
-            columns = [{"name": i, "id": i} for i in df.columns]
-            data = df.to_dict('records')
-            return data, columns
+            return html.Img(src=img_src, style={"maxWidth": "85%", "maxHeight": "85%"})
         else:
-            # Handle HTTP errors (e.g., file not found on the server)
-            return [], []
+            return html.Div("No data available for the selected date and tab.", style={"textAlign": "center", "color": "red", "fontWeight": "bold"})
     except requests.RequestException as e:
-        # Handle other request issues, such as network errors
-        print(f"Error fetching CSV: {e}")
-        return [], []
+        return html.Div(f"Error fetching image: {e}", style={"textAlign": "center", "color": "red", "fontWeight": "bold"})
+
+@app.callback(
+    Output('compare-tabs-content', 'children'),
+    [Input('compare-tabs', 'value'), Input('main-rfi-date-picker', 'date')]
+)
+def update_compare_tabs_content(compare_tab, selected_date):
+    if compare_tab == 'compare-1':
+        return update_compare_image('1', selected_date)
+    elif compare_tab == 'compare-2':
+        return update_compare_image('2', selected_date)
+    elif compare_tab == 'compare-3':
+        return update_compare_image('3', selected_date)
+    return html.Div("Select a date.", style={"textAlign": "center", "color": "red", "fontWeight": "bold"})
+
+def update_compare_image(tab_suffix, selected_date):
+    if not selected_date:
+        return html.Div("Select a date.", style={"textAlign": "center", "color": "red", "fontWeight": "bold"})
+
+    selected_date_obj = pd.to_datetime(selected_date)
+    formatted_date = selected_date_obj.strftime('%Y%m%d')  # Format date as YYYYMMDD
+
+    #check what tab is selected and edit the image source accordingly
+    if tab_suffix == '1':
+        img_src = f"http://universe.phys.unm.edu/data/waterfall_compare/sh1_{formatted_date}.png"
+    elif tab_suffix == '2':
+        img_src = f"http://universe.phys.unm.edu/data/waterfall_compare/sh2_{formatted_date}.png"
+    elif tab_suffix == '3':
+        img_src = f"http://universe.phys.unm.edu/data/waterfall_compare/anritsu_{formatted_date}.png"
+
+    try:
+        response = requests.get(img_src)
+        if response.status_code == 200:
+            return html.Img(src=img_src, style={"maxWidth": "85%", "maxHeight": "85%"})
+        else:
+            return html.Div("No data available for the selected date and tab.", style={"textAlign": "center", "color": "red", "fontWeight": "bold"})
+    except requests.RequestException as e:
+        return html.Div(f"Error fetching image: {e}", style={"textAlign": "center", "color": "red", "fontWeight": "bold"})
 
 @app.callback(
     Output('main-page-content', 'children'),
@@ -148,8 +154,8 @@ def update_data_table(tab, selected_date):
 def display_page(pathname):
     if pathname == '/frequency_explorer':
         return frequency_explorer.layout
-    #elif pathname == '/known_frequencies':
-        #return known_frequencies2.layout
+    # elif pathname == '/known_frequencies':
+        # return known_frequencies2.layout
     return layout_main_page()
 
 if __name__ == '__main__':
--- pages/frequency_explorer.py
+++ pages/frequency_explorer.py
@@ -1,157 +1,181 @@
-from dash import html, dcc, callback, State 
-from dash.dependencies import Input, Output 
-import dash_bootstrap_components as dbc 
-import plotly.graph_objs as go 
-import pandas as pd 
-import numpy as np 
-from scipy.stats import kurtosis, skew 
-from datetime import datetime 
-import plotly.figure_factory as ff 
-import requests 
-from io import StringIO 
+from dash import html, dcc, callback, State
+from dash.dependencies import Input, Output
+import dash_bootstrap_components as dbc
+import plotly.graph_objs as go
+from plotly.subplots import make_subplots
+import pandas as pd
+import numpy as np
+from scipy.stats import kurtosis, skew
+from datetime import datetime
+import plotly.figure_factory as ff
+import requests
+from io import StringIO
 
-def generate_card_content(stat, value): 
-    """Generate HTML content for a statistic card with Bootstrap classes.""" 
-    return dbc.Card( 
-        dbc.CardBody([ 
-            html.H6(stat, className="card-title"), 
-            html.P(f"{value:.2f}", className="card-text"), 
-        ]), 
-        className="m-2 text-center", 
-        style={"width": "140px", "backgroundColor": "#F9F9F9"} 
-    ) 
+def generate_card_content(stat, value):
+    """Generate HTML content for a statistic card with Bootstrap classes."""
+    return dbc.Card(
+        dbc.CardBody([
+            html.H6(stat, className="card-title"),
+            html.P(f"{value:.2f}", className="card-text"),
+        ]),
+        className="m-2 text-center",
+        style={"width": "140px", "backgroundColor": "#F9F9F9"}
+    )
 
-layout = html.Div([ 
-    html.H4("Frequency Explorer", className="text-center mb-4"), 
-    dbc.Row( 
-        dbc.Col([ 
-            html.Div([  # Flex container for inline elements 
-                dcc.RadioItems( 
-                    id='fe-signal-hound-input', 
-                    options=[ 
-                        {'label': 'SH1-MAPO', 'value': '1'}, 
-                        {'label': 'SH2-DSL', 'value': '2'} 
-                    ], 
-                    inline=False, 
-                    className="me-2",  # margin right to space elements 
-                ), 
-                dcc.DatePickerSingle( 
-                    id='fe-date-picker-single', 
-                    min_date_allowed=datetime(1995, 1, 1), 
-                    max_date_allowed=datetime.now(), 
-                    initial_visible_month=datetime.now(), 
-                    date=datetime.now().date(), 
-                    className="me-2" 
-                ), 
-                dcc.Input( 
-                    id="fe-frequency-input", 
-                    type="number", 
-                    placeholder="Frequency in GHz", 
-                    step=0.0001, 
-                    className="me-2" 
-                ), 
-                html.Button("Submit", id="fe-submit-button", n_clicks=0, className="btn btn-secondary"), 
-            ], className="d-flex justify-content-center align-items-center"),  # This ensures inline layout 
-        ], width=12), 
-        className="justify-content-center" 
-    ), 
-    html.Div(id="statistics-output", className="text-center mb-4 d-flex flex-wrap justify-content-center"), 
-    dcc.Graph(id="time-series-graph"), 
-    dcc.Graph(id="box-plot"), 
-    dcc.Graph(id="pdf-plot"), 
-    dcc.Graph(id="qq-plot"), 
-], className="text-center") 
- 
-def fetch_data_from_server(signal_hound_number, date_str, target_frequency_ghz): 
-    url = f"http://universe.phys.unm.edu/data/time_series_matrix_data/sh{signal_hound_number}/{date_str}/{target_frequency_ghz}" 
-    try: 
-        response = requests.get(url) 
-        if response.status_code == 200: 
-            # Wrap the JSON string in a StringIO object 
-            json_str_io = StringIO(response.text) 
-            data_df = pd.read_json(json_str_io, orient='records') 
-            return data_df 
-        else: 
-            print(f"Error fetching data: Status code {response.status_code}") 
-            return pd.DataFrame() 
-    except Exception as e: 
-        print(f"Exception while fetching data: {e}") 
-        return pd.DataFrame() 
+layout = html.Div([
+    html.H4("Frequency Explorer", className="text-center mb-4"),
+    dbc.Row([
+        dbc.Col(
+            dbc.Card(
+                dbc.CardBody([
+                    dcc.RadioItems(
+                        id='fe-signal-hound-input',
+                        options=[
+                            {'label': 'SH1-MAPO', 'value': '1'},
+                            {'label': 'SH2-DSL', 'value': '2'}
+                        ],
+                        inline=False,
+                        className="me-2"
+                    )
+                ]),
+                className="mb-2"
+            ),
+            width="auto"
+        ),
+        dbc.Col(
+            dbc.Card(
+                dbc.CardBody([
+                    dcc.DatePickerSingle(
+                        id='fe-date-picker-single',
+                        min_date_allowed=datetime(1995, 1, 1),
+                        max_date_allowed=datetime.now(),
+                        initial_visible_month=datetime.now(),
+                        date=datetime.now().date(),
+                        className="me-2"
+                    )
+                ]),
+                className="mb-2"
+            ),
+            width="auto"
+        ),
+        dbc.Col(
+            dbc.Card(
+                dbc.CardBody([
+                    dcc.Input(
+                        id="fe-frequency-input",
+                        type="number",
+                        placeholder="Frequency in GHz",
+                        step=0.0001,
+                        className="me-2"
+                    )
+                ]),
+                className="mb-2"
+            ),
+            width="auto"
+        ),
+        dbc.Col(
+            dbc.Card(
+                dbc.CardBody([
+                    html.Button("Submit", id="fe-submit-button", n_clicks=0, className="btn btn-secondary")
+                ]),
+                className="mb-4"
+            ),
+            width="auto"
+        )
+    ], className="justify-content-center"),
+    html.Div(id="statistics-output", className="text-center mb-4 d-flex flex-wrap justify-content-center"),
+    dbc.Row([
+        dbc.Col(dbc.Card(dbc.CardBody(dcc.Graph(id="time-series-graph"))), width=12, className="mb-4"),
+        dbc.Col(dbc.Card(dbc.CardBody(dcc.Graph(id="combined-plot", style={"height": "800px"}))), width=12, className="mb-4"),
+    ], className="justify-content-center")
+], className="container")
 
-@callback( 
-    [Output('time-series-graph', 'figure'), 
-     Output('box-plot', 'figure'), 
-     Output('pdf-plot', 'figure'), 
-     Output('qq-plot', 'figure'), 
-     Output('statistics-output', 'children')], 
-    [Input('fe-submit-button', 'n_clicks')], 
-    [State('fe-signal-hound-input', 'value'), 
-     State('fe-date-picker-single', 'date'), 
-     State('fe-frequency-input', 'value'),] 
-) 
-def update_graphs_and_stats(n_clicks, signal_hound_number, date_str, frequency): 
-    if n_clicks < 1 or frequency is None or signal_hound_number is None: 
-        return [{}, {}, {}, {}, "Enter all fields and click submit."] 
-    formatted_date_str = datetime.strptime(date_str, "%Y-%m-%d").strftime("%Y%m%d") 
-    df = fetch_data_from_server(signal_hound_number, formatted_date_str, frequency) 
-    if df.empty: 
-        return [{}, {}, {}, {}, "No data available for the selected parameters."] 
+def fetch_data_from_server(signal_hound_number, date_str, target_frequency_ghz):
+    url = f"http://universe.phys.unm.edu/data/time_series_matrix_data/sh{signal_hound_number}/{date_str}/{target_frequency_ghz}"
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            # Wrap the JSON string in a StringIO object
+            json_str_io = StringIO(response.text)
+            data_df = pd.read_json(json_str_io, orient='records')
+            return data_df
+        else:
+            print(f"Error fetching data: Status code {response.status_code}")
+            return pd.DataFrame()
+    except Exception as e:
+        print(f"Exception while fetching data: {e}")
+        return pd.DataFrame()
 
-    # Time Series Graph using WebGL 
-    time_series_fig = go.Figure( 
-        data=[go.Scattergl(x=df['Timestamp'], y=df['Power (dBm)'], mode='lines+markers', name='Power')] 
-    ) 
-    time_series_fig.update_layout( 
-        title="Time Series of Power", 
-        xaxis_title="Time", 
-        yaxis_title="Power (dBm)", 
-        xaxis=dict(tickformat="%H:%M:%S") 
-    ) 
+@callback(
+    [Output('time-series-graph', 'figure'),
+     Output('combined-plot', 'figure'),
+     Output('statistics-output', 'children')],
+    [Input('fe-submit-button', 'n_clicks')],
+    [State('fe-signal-hound-input', 'value'),
+     State('fe-date-picker-single', 'date'),
+     State('fe-frequency-input', 'value'),]
+)
+def update_graphs_and_stats(n_clicks, signal_hound_number, date_str, frequency):
+    if n_clicks < 1 or frequency is None or signal_hound_number is None:
+        return [{}, {}, "Enter all fields and click submit."]
+    formatted_date_str = datetime.strptime(date_str, "%Y-%m-%d").strftime("%Y%m%d")
+    df = fetch_data_from_server(signal_hound_number, formatted_date_str, frequency)
+    if df.empty:
+        return [{}, {}, "No data available for the selected parameters."]
 
-    # Box Plot 
-    box_plot_fig = go.Figure( 
-        data=[go.Box(x=df['Power (dBm)'], name='Power', orientation='h')] 
-    ) 
+    # Time Series Graph using WebGL
+    time_series_fig = go.Figure(
+        data=[go.Scattergl(x=df['Timestamp'], y=df['Power (dBm)'], mode='lines+markers', name='Power')]
+    )
+    time_series_fig.update_layout(
+        title="Time Series of Power",
+        xaxis_title="Time",
+        yaxis_title="Power (dBm)",
+        xaxis=dict(tickformat="%H:%M:%S")
+    )
 
-    box_plot_fig.update_layout( 
-        title="Power Distribution Box Plot", 
-        yaxis_title="Power", 
-        xaxis_title="Power (dBm)" 
-    ) 
+    # Combined Box Plot and PDF Plot
+    combined_fig = make_subplots(
+        rows=2, cols=1,
+        shared_xaxes=True,
+        row_heights=[0.3, 0.5],
+        vertical_spacing=0.02
+    )
 
-    # PDF Plot 
-    pdf_plot_fig = ff.create_distplot( 
-        [df['Power (dBm)'].values], ['Power'], show_hist=False, show_rug=False 
-    ) 
-    pdf_plot_fig.update_layout( 
-        title="Probability Density Function of Power" 
-    ) 
+    # Adding Box plot
+    combined_fig.add_trace(
+        go.Box(x=df['Power (dBm)'], name='Power', marker_color='blue', boxmean=False),
+        row=1, col=1
+    )
 
-    # QQ Plot 
-    qq_data = np.sort(df['Power (dBm)'].values) 
-    theoretical_quantiles = np.sort(np.random.normal(np.mean(qq_data), np.std(qq_data), len(qq_data))) 
-    qq_plot_fig = go.Figure( 
-        data=[go.Scattergl(x=theoretical_quantiles, y=qq_data, mode='markers', name='Quantiles')] 
-    ) 
-    qq_plot_fig.update_layout( 
-        title="Q-Q Plot of Power Distribution", 
-        xaxis_title="Theoretical Quantiles", 
-        yaxis_title="Sample Quantiles" 
-    ) 
+    # Adding PDF plot
+    hist_data = [df['Power (dBm)']]
+    group_labels = ['Power']
+    fig_dist = ff.create_distplot(hist_data, group_labels, show_hist=False, show_rug=False)
 
-    # Statistics 
-    stats_describe = df['Power (dBm)'].describe() 
-    additional_stats = pd.Series({ 
-        "Skewness": skew(df['Power (dBm)'].values), 
-        "Kurtosis": kurtosis(df['Power (dBm)'].values, fisher=True) 
-    }) 
+    for trace in fig_dist['data']:
+        combined_fig.add_trace(trace, row=2, col=1)
 
-    stats_all = pd.concat([stats_describe, additional_stats]) 
+    combined_fig.update_layout(
+        title="Power Distribution and Density",
+        xaxis2_title="Power (dBm)",
+        yaxis=dict(title="Density"),
+        showlegend=False
+    )
 
-    stats_output = html.Div( 
-        [generate_card_content(stat, value) for stat, value in stats_all.items()], 
-        style={"textAlign": "center", "display": "flex", "flexWrap": "wrap", "justifyContent": "center"} 
-    ) 
+    # Statistics
+    stats_describe = df['Power (dBm)'].describe()
+    additional_stats = pd.Series({
+        "Skewness": skew(df['Power (dBm)'].values),
+        "Kurtosis": kurtosis(df['Power (dBm)'].values, fisher=True)
+    })
 
-    # Return the updated figures and stats content 
-    return time_series_fig, box_plot_fig, pdf_plot_fig, qq_plot_fig, stats_output 
+    stats_all = pd.concat([stats_describe[['count', 'min', 'max', 'mean']], additional_stats])
+
+    stats_output = html.Div(
+        [generate_card_content(stat, value) for stat, value in stats_all.items()],
+        style={"textAlign": "center", "display": "flex", "flexWrap": "wrap", "justifyContent": "center"}
+    )
+
+    # Return the updated figures and stats content
+    return time_series_fig, combined_fig, stats_output
